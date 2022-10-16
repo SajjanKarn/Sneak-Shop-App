a9a01716482a05230ae3ab3a0aeeab05ff5927d8
SajjanKarn <karnaa787@gmail.com>
2022-10-16 08:14:50 +0545

    added new arrival section

---
+++ src/components/FilterTabs.js
@@ -0,0 +1,80 @@
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
+import { height, totalSize, width } from "react-native-dimension";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import colors from "../../config/colors";
+
+const Tabs = ({ text = "All Product", active = false }) => (
+  <TouchableOpacity
+    activeOpacity={0.5}
+    style={[styles.tab, active ? styles.activeTab : null]}
+  >
+    <Text style={[styles.tabText, active ? styles.activeTabText : null]}>
+      {text}
+    </Text>
+  </TouchableOpacity>
+);
+
+export default function FilterTabs() {
+  const tabs = [
+    { id: 1, text: "All Product", active: true },
+    { id: 2, text: "Addidas" },
+    { id: 3, text: "Nike" },
+    { id: 4, text: "Puma" },
+    { id: 5, text: "Reebok" },
+    { id: 6, text: "New Balance" },
+    { id: 7, text: "Vans" },
+    { id: 8, text: "Converse" },
+    { id: 9, text: "Fila" },
+    { id: 10, text: "Asics" },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Tabs text={item.text} active={item.active} />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  tab: {
+    backgroundColor: colors.white,
+    paddingVertical: height(1.2),
+    paddingHorizontal: width(10),
+    borderRadius: width(10),
+    borderWidth: 1.3,
+    borderColor: "#E6E6E6",
+    marginHorizontal: width(1),
+  },
+  tabText: {
+    color: colors.black,
+    fontSize: totalSize(1.8),
+    fontFamily: "SFPro-Regular",
+  },
+  activeTab: {
+    backgroundColor: colors.primary,
+    borderColor: colors.primary,
+  },
+  activeTabText: {
+    color: colors.white,
+  },
+});
--- src/screens/UserNavigator/HomeScreen.js
+++ src/screens/UserNavigator/HomeScreen.js
@@ -4,20 +4,37 @@ import colors from "../../../config/colors";
 
 import { AntDesign, Feather } from "@expo/vector-icons";
 import SearchInput from "../../components/SearchInput";
+import FilterTabs from "../../components/FilterTabs";
 
 export default function HomeScreen() {
   return (
     <ScrollView style={styles.container}>
+      {/* ======= header ======= */}
       <View style={styles.headerUserCartContainer}>
         <Feather name="align-left" size={25} color={colors.primary} />
         <AntDesign name="shoppingcart" size={25} color={colors.primary} />
       </View>
+
+      {/* ====== SearchInput ====== */}
       <View style={styles.searchInputContainer}>
         <View style={styles.searchInput}>
           <SearchInput />
         </View>
         <View style={styles.filterIcon}>
           <AntDesign name="filter" size={25} color={colors.primary} />
+        </View>
+      </View>
+
+      {/* ====== filter tabs =====  */}
+      <View style={styles.filterTabsContainer}>
+        <FilterTabs />
+      </View>
+
+      {/* ====== new arrivals ====== */}
+      <View style={styles.newArrivalsContainer}>
+        <View style={styles.newArrivalsHeader}>
+          <Text style={styles.newArrivalsHeaderText}>New Arrivals</Text>
+          <Text style={styles.newArrivalsHeaderSeeAll}>See All</Text>
         </View>
       </View>
     </ScrollView>
@@ -36,6 +53,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: height(2),
   },
+
+  // search section
   searchInputContainer: {
     flexDirection: "row",
     alignItems: "center",
@@ -53,5 +72,27 @@ const styles = StyleSheet.create({
     marginLeft: width(2),
     borderWidth: 1.3,
     borderColor: "#E6E6E6",
+  },
+
+  // new arrivals
+  newArrivalsContainer: {
+    marginTop: height(4),
+  },
+  newArrivalsHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  newArrivalsHeaderText: {
+    fontSize: totalSize(3),
+    fontFamily: "Gilroy-Bold",
+    color: colors.primary,
+  },
+  newArrivalsHeaderSeeAll: {
+    fontSize: totalSize(1.8),
+    fontFamily: "SFPro-Medium",
+    color: colors.primary,
+    textDecorationLine: "underline",
+    textDecorationColor: colors.primary,
   },
 });
